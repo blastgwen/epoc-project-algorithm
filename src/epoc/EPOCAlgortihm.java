2ebc42f6306379229d96466fa2de3667dd72a44f
blastgwen <blastgwen@gmail.com>
2014-10-13 09:47:19 +0200

    commit pour csv

--- src/epoc/EPOCAlgortihm.java
+++ src/epoc/EPOCAlgortihm.java
@@ -1,10 +1,30 @@
 package epoc;
 
-public class EPOCAlgortihm {
+import java.util.List;
 
+import epoc.impl.JobB;
+import epoc.impl.JobT;
+
+public class EPOCAlgortihm {
+	private int indexExecution = 0;
+	
+	private List<JobB> listJobB;
+	private List<JobT> listJobT;
+	private List<Integer> listGreenEnergy;
+	
+	private int wastedEnergy = 0;
+	
+	/**
+	 * Initialise le programme et les données
+	 * Importe les différents fichiers
+	 */
+	private void initProgram(){
+		
+	}
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		EPOCAlgortihm epoc = new EPOCAlgortihm();
+		epoc.initProgram();
 
 	}
 
--- src/epoc/impl/JobB.java
+++ src/epoc/impl/JobB.java
@@ -7,6 +7,7 @@ import epoc.Job;
 public class JobB implements Job{
 	private List<Integer> charges;
 	private int maxJours;
+	private int indexExecution = 0;
 	
 	@Override
 	public List<Integer> getCharges() {
@@ -20,5 +21,17 @@ public class JobB implements Job{
 	public JobB(List<Integer> list, int jours){
 		this.charges = list;
 		this.maxJours = jours;
+	}
+	
+	public void incrementerExecution(){
+		this.indexExecution ++;
+	}
+	
+	public int getIndexExecution(){
+		return this.indexExecution;
+	}
+	
+	public int getNombreExecutionRestante(){
+		return this.charges.size() - indexExecution; 
 	}
 }
